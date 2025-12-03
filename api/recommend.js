545a2c268eb65c6a434889f7c1688dd491637fd6
koreyfunding-oss <koreyfunding@gmail.com>
2025-12-03 02:23:16 -0700

    Add placeholder recommend eAdd recommend API endpoint placeholderndpoint in API

---
+++ api/recommend.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+// Placeholder recommend endpoint
+router.post('/', (req, res) => {
+  res.json({ message: 'Recommend endpoint not implemented yet.' });
+});
+
+module.exports = router;
